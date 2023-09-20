ad1ee8dd2a20a81d56752d30800035686bdcb122
leulmintesinot <leulminti@gmail.com>
2023-09-20 01:57:48 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies an inputted number of string
+ * @dest: buffer storing the string copy
+ * @src: source string
+ * @n: maximum number of bytes to be copied from src
+ * Return: Pointer to resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	{
+		src_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}

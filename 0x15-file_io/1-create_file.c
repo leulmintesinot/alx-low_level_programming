afe24d625dbc2439bcca3570e0347645a577ab24
leulmintesinot <leulminti@gmail.com>
2023-10-30 02:47:22 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - create and write into file
+ * @filename: to be created
+ * @text_content: content to be copied
+ * Return: 1 if success and -1 if fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}

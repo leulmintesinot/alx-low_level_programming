e1cc6f3b85076cb475c9f31a990b9e30c6f67993
leulmintesinot <leulminti@gmail.com>
2023-10-30 02:58:41 -0700

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * append_text_to_file - add text to an existing file
+ * @filename: to append content to
+ * @text_content: to be appended to the file
+ * Return: 1 if success and -1 if not
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open (filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+	close (fd);
+
+	return (1);
+}

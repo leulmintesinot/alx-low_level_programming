842a065489b94fc33aa2e5977f840e427a8bde36
leulmintesinot <leulminti@gmail.com>
2023-12-08 06:35:49 -0800

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees double linked list
+ * @head: head of the list
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
